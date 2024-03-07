aa992fa5b4b30bf9e3789f76902d595a250490d2
Dave LeRoy <leroy.dave@gmail.com>
2024-03-07 14:09:26 -0800

    Attempt at adding a query for race results for a specific ID. NOT tested. Need to resolve issues that arise.

--- backend/src/resolvers/bayareatri_resolver.py
+++ backend/src/resolvers/bayareatri_resolver.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, insert
+from sqlalchemy import select
 
 from db.db import get_session
 from models import bayareatri_model
@@ -8,14 +8,25 @@ from scalars.race_scalar import Race
 async def get_races():
     """ Get all races resolver """
     async with get_session() as s:
-        sql = select(bayareatri_model.RRaceitem).order_by(bayareatri_model.RRaceitem.r_RaceId)
+        sql = select(bayareatri_model.Raceitem).order_by(bayareatri_model.Raceitem.r_RaceId)
         db_races = (await s.execute(sql)).scalars().unique().all()
     race_list = []
     for races in db_races:
-        race_dict = get_valid_data(races, bayareatri_model.RRaceitem)
+        race_dict = get_valid_data(races, bayareatri_model.Raceitem)
         print(race_dict)
         race_list.append(Race(**race_dict))
     return race_list
+
+async def get_results_by_id(id: int):
+    """ Get all results by id resolver """
+    async with get_session() as s:
+        sql = select(bayareatri_model.Resultitem).where(bayareatri_model.Result.r_RaceId == id)
+        db_results = (await s.execute(sql)).scalars().unique().all()
+    result_list = []
+    for results in db_results:
+        result_dict = get_valid_data(results, bayareatri_model.Resultitem)
+    result_list.append(Result(**result_dict))
+    return result_list
 
 def get_valid_data(model_data_object, model_class):
     data_dict = {}
--- backend/src/schemas/query.py
+++ backend/src/schemas/query.py
@@ -2,8 +2,9 @@ from typing import List
 
 import strawberry
 
-from resolvers.bayareatri_resolver import get_races
+from resolvers.bayareatri_resolver import get_races, get_results_by_id
 from scalars.race_scalar import Race
+from scalars.result_scalar import Result
 
 
 @strawberry.type
@@ -15,3 +16,8 @@ class Query:
         race_data_list = await get_races()
         return race_data_list
 
+    @strawberry.field
+    async def results(self, id: int) -> List[Result]:
+        """ Get all results """
+        result_data_list = await get_results(id)
+        return result_data_list
