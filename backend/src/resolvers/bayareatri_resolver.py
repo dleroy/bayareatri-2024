3efcd9dc83c464757fb7001c4c55ad827e2c135a
dleroy <leroy.dave@gmail.com>
2024-03-11 19:34:51 -0700

    Added user race query

--- backend/src/resolvers/bayareatri_resolver.py
+++ backend/src/resolvers/bayareatri_resolver.py
@@ -5,6 +5,7 @@ from models import bayareatri_model
 
 from scalars.race_scalar import Race
 from scalars.result_scalar import Result
+from scalars.venue_scalar import Venue
 
 
 
@@ -30,6 +31,16 @@ async def get_results_by_id(raceid: int):
         result_list.append(Result(**result_dict))
     return result_list
 
+async def get_user_results_by_name(firstname: str, lastname: str):
+    """ Get all user results resolver """
+    async with get_session() as s:
+        sql = select(bayareatri_model.RResultitem).where(bayareatri_model.RResultitem.r_FirstName.ilike(firstname) & bayareatri_model.RResultitem.r_LastName.ilike(lastname))
+        db_results = (await s.execute(sql)).scalars().unique().all()
+    result_list = []
+    for races in db_results:
+        result_dict = get_valid_data(races, bayareatri_model.RResultitem)
+        result_list.append(Result(**result_dict))
+    return result_list
 
 def get_valid_data(model_data_object, model_class):
     data_dict = {}
@@ -40,3 +51,22 @@ def get_valid_data(model_data_object, model_class):
             pass
     return data_dict
 
+#
+#Results query
+#$query = "select * from r_resultitem where r_LastName like '" . $lastn . "%' and r_FirstName like '" . $firstn . "%'
+#          order by CAST(r_RaceId as unsigned) DESC";
+
+
+#            $racename = find_racename_by_id($row['r_RaceId']);
+#    $venuequery = "select * from r_venueitem
+#                 where r_VenueId = '$venueid->r_RaceVenueId'";                                          
+
+#            $raceinfo = find_raceinfo_by_id($row['r_RaceId']);
+# // We retreive race date in 2 different printable formats, different from way stored in MySQL
+#    // fulldate:  June 21, 2005
+#    // fmtdate:   06/21/2005
+#    // MySQL default  2005-06-21
+#    $infoquery = 'select *, date_format( r_RaceDate, \'%M %e, %Y\' ) as fulldate' .
+#        ', date_format( r_RaceDate, \'%m/%d/%Y\' ) as fmtdate' .
+#        ' from r_raceitem where' .
+#        " r_RaceID='$raceid'";
