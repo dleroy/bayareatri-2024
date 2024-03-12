de93607569a169fec6de3e950c61ec2bb42f2177
dleroy <leroy.dave@gmail.com>
2024-03-11 19:34:29 -0700

    Staged but didn't uncomment nested queries by pointing the race scalars to a venue

--- backend/src/scalars/race_scalar.py
+++ backend/src/scalars/race_scalar.py
@@ -1,10 +1,12 @@
+import typing
 import strawberry
-
+#from venue_scalar import Venue
 
 @strawberry.type
 class Race:
     r_RaceId: int
-    r_RaceVenueId: int 
+#  r_RaceVenueId: "Venue"
+    r_RaceVenueId: int
     r_AgeData: int
     r_SplitData: int
     r_TransData: int
