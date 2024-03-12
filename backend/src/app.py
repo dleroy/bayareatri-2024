b0d3d4e8bf14eb9b6124c825711a2e30abef549c
dleroy <leroy.dave@gmail.com>
2024-03-11 19:29:50 -0700

    Added CORS middleware for FastAPI. Will become important for production

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,5 +1,6 @@
 import strawberry
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from strawberry.fastapi import GraphQLRouter
 from strawberry.schema.config import StrawberryConfig
 
@@ -7,8 +8,19 @@ from schemas.query import Query
 
 schema = strawberry.Schema(query=Query, config=StrawberryConfig(auto_camel_case=False))
 
+origins = [
+    "*"
+]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 graphql_app = GraphQLRouter(schema)
 app.include_router(graphql_app, prefix="/graphql")
-
